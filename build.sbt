5bc02bdb5d7b430cc70e7ddc269909cc43e7281a
Suhku Huh <nineclue@gmail.com>
2021-06-08 16:59:09 +0900

    initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+scalaVersion := "3.0.0"
+
+libraryDependencies ++= Seq(
+    "org.typelevel" %% "cats-effect" % "3.1.1",
+    "com.github.vandeseer" % "easytable" % "0.8.4"
+)
---
+++ src/main/scala/simpletable.scala
@@ -0,0 +1,22 @@
+import org.apache.pdfbox.pdmodel.{PDDocument, PDPage, PDPageContentStream}
+import org.apache.pdfbox.pdmodel.common.PDRectangle
+import org.vandeseer.easytable.TableDrawer
+import org.vandeseer.easytable.settings.{BorderStyle, HorizontalAlignment}
+import org.vandeseer.easytable.structure.{Row, Table}
+import org.vandeseer.easytable.structure.cell.TextCell
+import cats.effect.kernel.Resource
+
+object SimpleTable:
+  def main(as: Array[String]): Unit = 
+    println("안녕? 여러분!")
+
+  def test1() = 
+    val pdoc = PDDocument()
+    val p1 = PDPage(PDRectangle.A4)
+    pdoc.addPage(p1)
+    val cstream = PDPageContentStream(pdoc, p1)
+    val t = Table.builder().
+        addColumnsOfWidth(200, 200).padding(2).
+        addRow(
+            Row.builder()
+        )
